d4440dbcee4b0bbb7c5decf7e565f60899dc1140
LumiereXyloto <740207968@qq.com>
2019-10-17 15:21:40 +0800

    feat: 🎸 完成对称二叉树

---
+++ code/btree/symmetry.js
@@ -0,0 +1,21 @@
+// 一棵树否为对称二叉树
+
+function isSymmetrical (root) {
+  if (root) {
+    return isSymmetricalTree(root.left, root.right)
+  }
+  return true
+}
+
+function isSymmetricalTree (node1, node2) {
+  if (!node1 && !node2) {
+    return true
+  }
+  if (!node1 || !node2) {
+    return false
+  }
+  if (node1.val !== node2.val) {
+    return false
+  }
+  return isSymmetricalTree(node1.left, node2.right) && isSymmetricalTree(node1.right, node2.left)
+}
