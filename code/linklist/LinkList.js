87a52d1b2084814ae8f3cf0bf6a97bb0d5845c97
LumiereXyloto <740207968@qq.com>
2019-10-22 22:31:06 +0800

    feat: 🎸 新增链表

---
+++ code/linklist/LinkList.js
@@ -0,0 +1,63 @@
+export class ListNode {
+  constructor (val, next) {
+    this.val = val
+    this.next = next
+  }
+}
+
+export class List {
+  constructor (arr) {
+    if (arr) {
+      this.head = new ListNode(arr[0], null)
+      let current = this.head
+      for (let i = 1; i < arr.length; i++) {
+        current.next = new ListNode(arr[i], null)
+        current = current.next
+      }
+    } else {
+      this.head = new ListNode(null, null)
+    }
+  }
+
+  /**
+   * 从 0 开始计算，找到包括head 头节点 在哪的位于 index 位置的节点
+   * @param { Number } index
+   * @return {ListNode}
+   */
+  find (index) {
+    let current = this.head
+    for (let i = 0; i < index; i++) {
+      current = current.next
+    }
+    return current
+  }
+
+  /**
+   * 在指定位置插入节点
+   * @param {any} value
+   * @param {number} index
+   */
+  insert (value, index) {
+    // 获取当前位置前一个节点
+    let pre = this.find(index - 1)
+    let next = new ListNode(value, pre.next)
+    pre.next = next
+  }
+
+  /**
+   *  删除指定位置的节点
+   * @param {number} index
+   */
+  delete (index) {
+    // 如果要删除头结点
+    if (index === 0) {
+      this.head = this.head.next
+    } else if (this.find(index) === null || this.find(index).val === null) {
+      throw Error('输入节点不存在！')
+    } else {
+      // 获取要删除节点的前一个节点
+      let pre = this.find(index - 1)
+      pre.next = pre.next.next
+    }
+  }
+}
